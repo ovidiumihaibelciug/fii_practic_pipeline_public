0e3a030598aa6bc6a4b7af5230cffa4cd45d77fc
Ovidiu-Mihai Belciug <ovidiumihaibelciug@gmail.com>
2024-04-06 10:40:07 +0300

    changed bucket name

--- backend.tf
+++ backend.tf
@@ -13,12 +13,11 @@ provider "aws" {
 # }
 
 
-
 module "s3-terraform-state" {
   source  = "cloudposse/s3-bucket/aws"
   version = "3.1.3"
 
-  bucket_name        = "fii-practic-975050094922-terraform-state"
+  bucket_name        = "s3-terraform-state-fii-practic-ovidiu1231231231"
   versioning_enabled = true
 }
 
